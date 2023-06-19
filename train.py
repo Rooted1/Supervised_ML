446adcbeef819fc987438e6235251e761b584c06
Rooted1 <ruthobey@gmail.com>
2023-06-19 15:50:06 -0500

    include pca component to see how model performance improved

--- train.py
+++ train.py
@@ -8,16 +8,16 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('CalMod_06222009.csv')
 data = data.dropna()
+data = data.drop(['id', 'Unnamed: 0'], axis=1)
 data = data.reset_index()
 print(data)
 
 # split data into features and labels
-features = data.iloc[:, 2:44]
+features = data.iloc[:, :-1]
 labels = data.iloc[:, -1]
-print(labels)
 
 # split data into training and test set
-X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, random_state=18)
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, random_state=10)
 print(X_test)
 
 # train and evaluate model using Random forest classifier and make predictions
@@ -29,8 +29,19 @@ print('Accuracy on test data is %.4f' % (accuracy_score(y_test, y_pred)))
 
 # use sklearn PCA to improve accuracy
 pca = PCA().fit(features)
-print(pca.explained_variance_ratio_)
+# print(pca.explained_variance_ratio_)
 plt.plot(np.cumsum(pca.explained_variance_ratio_))
 plt.xlabel('number of components')
 plt.ylabel('cumulative explained variance');
-plt.show()
+# plt.show()
+
+# specify components to improve results
+pca = PCA(n_components=2).fit(features)
+X_pca = pca.transform(features)
+X_train, X_test, y_train, y_test = train_test_split(X_pca, labels, test_size=0.3, random_state=10)
+
+# fit training model and predict performance
+clf = RandomForestClassifier()
+clf = clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+print('Accuracy on new test data is %.4f' % (accuracy_score(y_test, y_pred)))
