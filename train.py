b48f44311aaf669962f9df57ab9b49e375bdc1c5
Rooted1 <ruthobey@gmail.com>
2023-06-19 13:16:25 -0500

    separate data into features and labels

--- train.py
+++ train.py
@@ -4,3 +4,8 @@ data = pd.read_csv('CalMod_06222009.csv')
 data = data.dropna()
 data = data.reset_index()
 print(data)
+
+# split data into features and labels
+features = data.iloc[:, 2:44]
+labels = data.iloc[:, -1]
+print(labels)
