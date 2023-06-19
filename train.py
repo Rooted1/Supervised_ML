a71904f4f54ce1307b715d2f7264564cfd7b4bd8
Rooted1 <ruthobey@gmail.com>
2023-06-19 14:38:56 -0500

    improve accuracy using sklearn PCA

--- train.py
+++ train.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 data = pd.read_csv('CalMod_06222009.csv')
 data = data.dropna()
@@ -23,3 +26,11 @@ clf = clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 
 print('Accuracy on test data is %.4f' % (accuracy_score(y_test, y_pred)))
+
+# use sklearn PCA to improve accuracy
+pca = PCA().fit(features)
+print(pca.explained_variance_ratio_)
+plt.plot(np.cumsum(pca.explained_variance_ratio_))
+plt.xlabel('number of components')
+plt.ylabel('cumulative explained variance');
+plt.show()
