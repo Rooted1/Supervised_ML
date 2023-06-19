eda166e8f6b4687c834b9f8ab969c85eee2516c5
Rooted1 <ruthobey@gmail.com>
2023-06-19 13:34:05 -0500

    split data into train and test sets

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 data = pd.read_csv('CalMod_06222009.csv')
 data = data.dropna()
@@ -9,3 +10,7 @@ print(data)
 features = data.iloc[:, 2:44]
 labels = data.iloc[:, -1]
 print(labels)
+
+# split data into training and test set
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, random_state=18)
+print(X_test)
